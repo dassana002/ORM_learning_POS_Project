e1929b1589c919383e3100c29f0084c412c367b2
dassana002 <dassana002@gmail.com>
2026-02-25 11:47:57 +0530

    create main class.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,27 +7,6 @@ import org.hibernate.cfg.Configuration;
 
 public class Main {
     static void main() {
-        try {
-            // Introduce Hibernate Configurations to Project
-            SessionFactory sessionFactory = new Configuration()
-                    .configure("hibernate.configuration.xml")
-                    .addAnnotatedClass(Customer.class)
-                    .buildSessionFactory();
 
-            Session session = sessionFactory.openSession();
-            Transaction transaction = session.beginTransaction();
-
-            Customer customer = new Customer();
-            customer.setName("Thiwanka");
-            customer.setAddress("Horana");
-
-            session.save(customer);
-            transaction.commit();
-
-            session.close();
-            sessionFactory.close();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 }
