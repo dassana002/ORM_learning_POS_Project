f7ec5db3a9cbed4dfab8536b77a93940cf1c0421
dassana002 <dassana002@gmail.com>
2026-02-25 11:02:18 +0530

    download lombok tool and using Special Annotations in customer class. Lombok tool received us. we can use easy way for Encapsulate.

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -1,11 +1,18 @@
 package org.example;
 
-import jakarta.persistence.*;
+import jakarta.persistence.*; // JPA Specification
+import lombok.*;
 
-// Entity Mapping
+// Java Hibernate Annotations
+// Object and DB Entity Mapping
 @Entity
 // Table Name for DB
 @Table(name = "customers")
+
+// Lombok Annotations
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Customer {
 
     // Primary key
