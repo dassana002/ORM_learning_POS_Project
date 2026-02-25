d5b08467667c2f874c0fb9b90a29e8d762555322
dassana002 <dassana002@gmail.com>
2026-02-25 12:03:31 +0530

    Introduce Hibernate Configurations to Project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,5 +8,48 @@ import org.hibernate.cfg.Configuration;
 public class Main {
     static void main() {
 
+        // Introduce Hibernate Configurations to Project
+        try {
+            /**
+                * Create Hibernate configuration object.
+                * Create Session Factory (Hibernate engine)
+                * Load hibernate.configuration.xml file
+                * Introducing table to Hibernate.
+                * Configuration finalized and Session Factory Create
+            **/
+            SessionFactory sessionFactory = new Configuration()
+                    .configure("hibernate.configuration.xml")
+                    .addAnnotatedClass(Customer.class)
+                    .buildSessionFactory();
+
+            /**
+             *  Session Opening (Database Conversion)
+             * **/
+            Session session = sessionFactory.openSession();
+
+            /**
+             * Transaction begin (transaction ensures)
+             * **/
+            Transaction transaction = session.beginTransaction();
+
+            Customer customer = new Customer();
+            customer.setName("Thiwanka");
+            customer.setAddress("Horana");
+
+            // Object Save
+            session.save(customer);
+
+            // Transaction commit
+            transaction.commit();
+
+            // Session close
+            session.close();
+
+            // Session Factory close
+            sessionFactory.close();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
